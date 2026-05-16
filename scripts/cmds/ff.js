833684e9ca345856f65272b9ee20f1219a5e3877
kanazahid929 <puresingleking@gmail.com>
2026-05-16 12:34:58 +0600

    Update ff.js

--- scripts/cmds/ff.js
+++ scripts/cmds/ff.js
@@ -10,7 +10,7 @@ module.exports = {
 		countDown: 5,
 		role: 0,
 		shortDescription: "sarcasm",
-		longDescription: "Responds with random media when someone says 'acs",
+		longDescription: "Responds with random media when someone says ff",
 		category: "reply",
 	},
 
@@ -19,28 +19,31 @@ module.exports = {
 	onChat: async function ({ event, message }) {
 		if (event.body && event.body.toLowerCase() === "ff") {
 			const mediaLinks = [
-				"https://drive.google.com/uc?id=XTVyi8xIVZipq4A28V-tb52oKBtR8WIb"
-				
-				
+				"https://drive.google.com/uc?id=1eeRTnKPtDgcBIrC0IIXWFdhubXtDzOdM"
 			];
 
 			const randomLink = mediaLinks[Math.floor(Math.random() * mediaLinks.length)];
-			const fileName = path.basename(randomLink);
-			const filePath = path.join(__dirname, fileName);
+			const filePath = path.join(__dirname, "ff.mp4");
 
 			try {
-				const response = await axios.get(randomLink, { responseType: "arraybuffer" });
-				fs.writeFileSync(filePath, Buffer.from(response.data));
+				const response = await axios.get(randomLink, {
+					responseType: "arraybuffer"
+				});
+
+				fs.writeFileSync(filePath, response.data);
 
 				await message.reply({
-					body: "✨🖤 𝙈𝙔 𝙂𝘼𝙈𝙀 𝙐𝙄𝘿 👀🍫 :–2732681410\n\n𝙄𝙩'𝙨 𝙝𝙖𝙘𝙠𝙚𝙧 𝙪𝙣𝙞𝙩𝙮 👀🪄❗– 𝙒𝙝𝙤 𝙄 𝙖𝙢, 𝙮𝙤𝙪 𝙝𝙖𝙫𝙚 𝙣𝙤 𝙞𝙙𝙚𝙖 🖇️💚🚩\n\n\n𝘿𝙤𝙣'𝙩 𝙞𝙣𝙫𝙞𝙩𝙚 𝙢𝙚 𝙘𝙪𝙨𝙩𝙤𝙢 🚩\n\n\n🖤👀✨",
+					body: "✨🖤 𝙈𝙔 𝙂𝘼𝙈𝙀 𝙐𝙄𝘿 👀🍫 :–2732681410\n\n𝙄𝙩'𝙨 𝙝𝙖𝙘𝙠𝙚𝙧 𝙪𝙣𝙞𝙩𝙮 👀🪄❗– 𝙒𝙝𝙤 𝙄 𝙖𝙢, 𝙮𝙤𝙪 𝙝𝙖𝙫𝙚 𝙣𝙤 𝙞𝙙𝙚𝙖 🖇️💚🚩\n\n𝘿𝙤𝙣'𝙩 𝙞𝙣𝙫𝙞𝙩𝙚 𝙢𝙚 𝙘𝙪𝙨𝙩𝙤𝙢 🚩\n\n🖤👀✨",
 					attachment: fs.createReadStream(filePath)
 				});
+
 			} catch (err) {
 				console.error("Failed to fetch media:", err.message);
-				await message.reply("Sorry, couldn't load the media.");
+				await message.reply("Media load failed.");
 			} finally {
-				fs.unlink(filePath, () => {});
+				if (fs.existsSync(filePath)) {
+					fs.unlinkSync(filePath);
+				}
 			}
 		}
 	}
